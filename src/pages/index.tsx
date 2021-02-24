4a275711496fd764eeacc7eb8204e467feffa58d
berespt <piotr.beres@gmail.com>
2021-02-24 23:40:08 +0100

    Remove unused sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,8 @@ import { NextPage, GetStaticProps } from 'next';
 
 import SEO from 'components/home/SEO';
 import HeroSection from 'components/home/HeroSection';
-import BlogSection from 'components/home/BlogSection';
 import Layout from 'components/home/Layout';
 import FeatureSection from 'components/home/FeatureSection';
-import StepsSection from 'components/home/StepsSection';
-import TeamSection from 'components/home/TeamSection';
-import PricingSection from 'components/home/PricingSection';
 import { HomeAttributes } from 'interfaces/home';
 
 interface Props {
@@ -32,28 +28,6 @@ const HomePage: NextPage<Props> = ({ content }) => {
           title={attributes.feature_title}
           description={attributes.feature_description}
           features={attributes.features}
-        />
-        <StepsSection
-          version={attributes.steps_version}
-          steps={attributes.steps}
-          image={attributes.steps_image}
-        />
-        <PricingSection
-          title={attributes.pricing_title}
-          description={attributes.pricing_description}
-          plans={attributes.plans}
-        />
-        <TeamSection
-          version={attributes.team_version}
-          title={attributes.team_title}
-          description={attributes.team_description}
-          team={attributes.team}
-        />
-        <BlogSection
-          version={attributes.blog_version}
-          title={attributes.blog_title}
-          description={attributes.blog_description}
-          slugs={attributes.posts}
         />
       </Layout>
     </>
