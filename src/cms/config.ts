ca6c4a2b200dbca54e90e2640cce0aa2e1dcf259
berespt <piotr.beres@gmail.com>
2021-02-25 00:15:47 +0100

    Change backend repo to point to correct git repo

--- src/cms/config.ts
+++ src/cms/config.ts
@@ -2,7 +2,7 @@ export default {
   cms_manual_init: true,
   backend: {
     name: 'github',
-    repo: 'jakeprins/nextwind',
+    repo: 'berespt/ekostylbud-website',
     branch: 'master',
   },
   media_folder: 'public/img',
