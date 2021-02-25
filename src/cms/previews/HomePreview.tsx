25f2084fb85722b647bfe41959da4bd84c9962b4
berespt <piotr.beres@gmail.com>
2021-02-25 19:26:18 +0100

    Remove unused home sections

--- src/cms/previews/HomePreview.tsx
+++ src/cms/previews/HomePreview.tsx
@@ -1,10 +1,6 @@
 import { ComponentType } from 'react';
 import HeroSection from 'components/home/HeroSection';
 import ClientSection from 'components/home/ClientsSection';
-import StepsSection from 'components/home/StepsSection';
-import TeamSection from 'components/home/TeamSection';
-import PricingSection from 'components/home/PricingSection';
-import BlogSection from 'components/home/BlogSection';
 
 const HomePreview: ComponentType<any> = ({ entry, widgetsFor }) => {
   const clients = [];
@@ -12,39 +8,6 @@ const HomePreview: ComponentType<any> = ({ entry, widgetsFor }) => {
     clients.push({
       name: client?.getIn(['data', 'name']),
       description: client?.getIn(['data', 'description']),
-    });
-  });
-
-  const steps = [];
-  widgetsFor('steps').map((step) => {
-    steps.push({
-      name: step?.getIn(['data', 'name']),
-      description: step?.getIn(['data', 'description']),
-    });
-  });
-
-  const plans = [];
-  widgetsFor('plans').map((plan) => {
-    plans.push({
-      name: plan?.getIn(['data', 'name']),
-      description: plan?.getIn(['data', 'description']),
-      price: plan?.getIn(['data', 'price']),
-      usps: plan?.getIn(['data', 'usps']),
-    });
-  });
-
-  const slugs = [];
-  widgetsFor('posts').map((post) => {
-    slugs.push(post?.getIn(['data']));
-  });
-
-  const team = [];
-  widgetsFor('team').map((member) => {
-    team.push({
-      name: member?.getIn(['data', 'name']),
-      description: member?.getIn(['data', 'description']),
-      position: member?.getIn(['data', 'position']),
-      image: member?.getIn(['data', 'image']),
     });
   });
 
@@ -58,28 +21,6 @@ const HomePreview: ComponentType<any> = ({ entry, widgetsFor }) => {
       <ClientSection
         title={entry.getIn(['data', 'client_title'])}
         clients={clients}
-      />
-      <StepsSection
-        version={entry.getIn(['data', 'steps_version'])}
-        image={entry.getIn(['data', 'steps_image'])}
-        steps={steps}
-      />
-      <PricingSection
-        title={entry.getIn(['data', 'pricing_title'])}
-        description={entry.getIn(['data', 'pricing_description'])}
-        plans={plans}
-      />
-      <TeamSection
-        version={entry.getIn(['data', 'team_version'])}
-        title={entry.getIn(['data', 'team_title'])}
-        description={entry.getIn(['data', 'team_description'])}
-        team={team}
-      />
-      <BlogSection
-        version={entry.getIn(['data', 'blog_version'])}
-        title={entry.getIn(['data', 'blog_title'])}
-        description={entry.getIn(['data', 'blog_description'])}
-        slugs={slugs}
       />
     </>
   );
