a0dff877d1f80979dee5f87d7bff8509e2b48094
berespt <piotr.beres@gmail.com>
2021-03-17 23:45:44 +0100

    Get rid of console errors

--- src/components/home/ServicesSection/index.tsx
+++ src/components/home/ServicesSection/index.tsx
@@ -19,9 +19,9 @@ const ServicesSection: React.FC<Props> = ({
           <h2 className="text-3xl font-semibold">{title}</h2>
         </div>
         <div className="lg:w-1/2 px-2">
-          <p className="mb-12">
+          <div className="mb-12">
             <ReactMarkdown>{description}</ReactMarkdown>
-          </p>
+          </div>
           <div className="flex flex-wrap -mx-6">
             <div className="px-6 mb-6 lg:mb-0">
               <div className="w-1/2 text-xl mb-6 font-semibold">
@@ -29,7 +29,9 @@ const ServicesSection: React.FC<Props> = ({
               </div>
               <ul className="grid grid-cols-2 gap-x-12 md:gap-x-36 gap-y-6">
                 {services.map((service) => (
-                  <li className="text-sm">{service}</li>
+                  <li key={service} className="text-sm">
+                    {service}
+                  </li>
                 ))}
               </ul>
             </div>
