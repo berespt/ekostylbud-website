15f09d0666f82f1b5bb2d46d3717879ee8ee5675
berespt <piotr.beres@gmail.com>
2021-03-19 01:23:28 +0100

    Change spacing

--- src/components/home/ClientsSection/index.tsx
+++ src/components/home/ClientsSection/index.tsx
@@ -9,8 +9,8 @@ interface Props {
 const ClientsSection: React.FC<Props> = ({ title, clients }) => {
   return (
     <section className="text-gray-700 body-font" id="clients">
-      <div className="container px-5 py-24 mx-auto">
-        <div className="mb-20 text-center">
+      <div className="container px-5 pt-10 mx-auto">
+        <div className="mb-10 text-center">
           <h1 className="mb-4 text-2xl font-bold text-gray-900 sm:text-3xl lg:text-4xl title-font">
             {title}
           </h1>
