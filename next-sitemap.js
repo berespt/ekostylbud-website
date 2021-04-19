32404edf32e9516671977cec977459d4f79894f6
berespt <piotr.beres@gmail.com>
2021-04-19 18:52:22 +0200

    Change sitemap priorities

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,22 @@
 module.exports = {
   siteUrl: 'https://ekostylbud.pl',
   generateRobotsTxt: true,
+  priority: 0.5,
   exclude: ['/admin'],
+  transform: async (config, path) => {
+    if ('/' === path) {
+      return {
+        loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+        changefreq: config.changefreq,
+        priority: 1,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      };
+    }
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+    };
+  },
 };
